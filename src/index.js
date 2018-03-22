bc9b75708b6939db22ec49ef1f76b72470e1cf29
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2018-03-22 01:56:48 -0300

    feature(parte1-resuelta): Se agrega codigo que resuelve la parte 1 del proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import 'whatwg-fetch';
 import { ApiRequest } from './dataRequest';
 
+function formatColumnName(name) {
+    return `${name[0].toUpperCase()}${name.slice(1)}`;
+}
+function createColumns(columnNames) {
+    const row = document.createDocumentFragment();
+    let thead;
+    ['Currency', ...columnNames].forEach(name => {
+        thead = document.createElement('th');
+        thead.textContent = name;
+        row.appendChild(thead);
+    }); 
+    return row;
+}
 function createTable(responseData){
     const tableData = responseData;
     
@@ -9,6 +22,14 @@ function createTable(responseData){
     //Parte 1 Tarea
     //Implementar código para crear la tabla basada en la data acá
     //No olvidar agregar el código HTML necesario en el archivo index.html
+    const table = document.querySelector('.home-table__region');
+    const theadRow = table.querySelector('thead tr');
+    const parsedData = JSON.parse(tableData);
+    const currencyNames = Object.keys(parsedData);
+    const sampleKey = currencyNames[0];
+    const columnNames = Object.keys(parsedData[sampleKey]).map(formatColumnName);
+
+    theadRow.appendChild(createColumns(columnNames));
 }
 
 /*
