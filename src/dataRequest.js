3936a26b9d72275969319f4157592c541fc93c66
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2018-05-14 10:34:55 -0400

    chore: improve number formatter flow in data request

--- src/dataRequest.js
+++ src/dataRequest.js
@@ -1,6 +1,5 @@
 const currenciesUrl = 'https://blockchain.info/es/ticker';
 const balanceUrl = `https://chain.so/api/v2/get_address_balance/BTC/${BITCOIN_CONFIG.address}`;
-//  const mockResponse = require('../stubs/api_response.json');
 const numberFormater = number => {
   const numberSplitted = number.toString().split('.');
   const replaceChunksRegex = /[0-9](?=(?:[0-9]{3})+(?![0-9]))/g;
@@ -10,23 +9,23 @@ const numberFormater = number => {
 };
 
 export const ApiRequest = () => {
-    //  return Promise.resolve(mockResponse);
   const getCurrenciesValues = fetch(currenciesUrl)
     .then(response => response.text())
     .then(data => JSON.parse(data))
-    .then(data => {
-      const formatedData = {};
-      Object.keys(data).forEach(currency => {
-        formatedData[currency] = {
-          ...data[currency],
-          '15m': numberFormater(data[currency]['15m']),
-          'last': numberFormater(data[currency]['last']),
-          'buy': numberFormater(data[currency]['buy']),
-          'sell': numberFormater(data[currency]['sell']),
-        }
-      });
-      return formatedData;
-    });
+    .then(data => Object.keys(data)
+      .reduce((formatedData, currency) => 
+        ({
+          ...formatedData,
+          [currency]: {
+            ...data[currency],
+            '15m': numberFormater(data[currency]['15m']),
+            'last': numberFormater(data[currency]['last']),
+            'buy': numberFormater(data[currency]['buy']),
+            'sell': numberFormater(data[currency]['sell'])
+          }
+        }),
+        {})
+    );
   const getBalance = fetch(balanceUrl)
     .then(response => response.text())
     .then(response => JSON.parse(response).data);
