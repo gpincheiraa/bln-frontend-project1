132ec318d89ba2bf0c3d3b8e1485475a5482e875
gigashock3u <gigashock3u@hotmail.com>
2018-06-07 19:28:47 -0400

    code reduce

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import 'whatwg-fetch';
 import { getCurrenciesValues,  getBalance } from './dataRequest';
-import { initializeTable } from './table';
-import { initializeBalance } from './balance';
+import { onlineBalance, offlineBalance, onlineTable, offlineTable} from './storedData';
 import { globalsDefinitions } from './utils';
 
 const offlinePlugin = require('offline-plugin/runtime');
 
-function onlineBalance(data) {
-    localStorage.setItem("balanceData", JSON.stringify(data))
-    initializeBalance(data);
-}
-
-function offlineBalance(data) {
-    let variable = JSON.parse(localStorage.getItem("balanceData"));
-    initializeBalance(variable);
-}
-
-function onlineTable(data) {
-    localStorage.setItem("tableData", JSON.stringify(data));
-    initializeTable(data);
-}
-
-function offlineTable(data) {
-    let variable = JSON.parse(localStorage.getItem("tableData"));  
-    initializeTable(variable);
-}
-
-function callGets () {
+function callGets() {
     getBalance()
     .then(onlineBalance)
     .catch(offlineBalance);
