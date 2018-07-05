2538952d5667f61ddb20f6df555781437adeaf78
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2018-07-04 20:13:49 -0400

    codacy issues resolved

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OfflinePlugin = require('offline-plugin')
+const OfflinePlugin = require('offline-plugin');
 
 const { author, config , version } = require('./package.json');
 const { address } = require('./btc-config.json');
